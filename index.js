407f564db224a2ac015924fa0b57e9bc1197a7fe
AdamDiment <adam_diment@hotmail.com>
2016-11-04 15:57:51 +0200

    reorder

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-  res.redirect('index.html');
+  res.redirect('/index.html');
 });
 
 app.listen(process.env.PORT || 3000, function(){
